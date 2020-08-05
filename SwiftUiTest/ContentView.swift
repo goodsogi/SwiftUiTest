ddb9945ca601b11478e3eabeab8a141b8177e2f7
Jeonggyu Park <goodsogi@gmail.com>
2020-08-05 14:29:28 +0900

    소스 커밋

---
+++ SwiftUiTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiTest
+//
+//  Created by Jeonggyu Park on 05/08/2020.
+//  Copyright © 2020 Jeonggyu Park. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
